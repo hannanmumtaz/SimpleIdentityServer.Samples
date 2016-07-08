c0bb480cd1d9dfbb0235b41e249e4359bd80c796
thabart <habarthierry@hotmail.fr>
2016-07-08 14:39:01 +0200

    Add API project

---
+++ ClientApi/Controllers/ClientsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ClientApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClientsController : Controller
+    {
+        #region Public methods
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "client_1", "client_2" };
+        }
+
+        #endregion
+    }
+}
---
+++ ClientApi/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace ClientApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:5100")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
