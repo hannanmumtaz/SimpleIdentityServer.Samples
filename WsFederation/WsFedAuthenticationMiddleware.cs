921761a1b88ed8261e7964788689fdc5c671fed9
thabart <habarthierry@hotmail.fr>
2016-07-13 13:35:59 +0200

    Add WsFederation

---
+++ WsFederation/WsFedAuthenticationMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Text.Encodings.Web;
+
+namespace WsFederation
+{
+    public class WsFedAuthenticationMiddleware : AuthenticationMiddleware<WsFedAuthenticationOptions>
+    {
+
+        public WsFedAuthenticationMiddleware(
+            RequestDelegate next,
+            IDataProtectionProvider dataProtectionProvider,
+            ILoggerFactory loggerFactory,
+            UrlEncoder urlEncoder,
+            IOptions<WsFedAuthenticationOptions> options
+            ) : base(next, options, loggerFactory, urlEncoder)
+        {
+
+            if (dataProtectionProvider == null)
+            {
+                throw new ArgumentNullException(nameof(dataProtectionProvider));
+            }
+
+            if (Options.Events == null)
+            {
+                Options.Events = new CookieAuthenticationEvents();
+            }
+
+            if (String.IsNullOrEmpty(Options.CookieName))
+            {
+                Options.CookieName = CookieAuthenticationDefaults.CookiePrefix + Options.AuthenticationScheme;
+            }
+
+            if (Options.TicketDataFormat == null)
+            {
+                var provider = Options.DataProtectionProvider ?? dataProtectionProvider;
+                var dataProtector = provider.CreateProtector(typeof(CookieAuthenticationMiddleware).FullName, Options.AuthenticationScheme, "v2");
+                Options.TicketDataFormat = new TicketDataFormat(dataProtector);
+            }
+
+            if (Options.CookieManager == null)
+            {
+                Options.CookieManager = new ChunkingCookieManager();
+            }
+
+
+            if (!Options.AccessDeniedPath.HasValue)
+            {
+                Options.AccessDeniedPath = CookieAuthenticationDefaults.AccessDeniedPath;
+            }
+
+        }
+
+        protected override AuthenticationHandler<WsFedAuthenticationOptions> CreateHandler()
+        {
+            return new WsFedAuthenticationHandler();
+        }
+    }
+}
---
+++ WsFederation/WsFedAuthenticationOptions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace WsFederation
+{
+    public class WsFedAuthenticationOptions : CookieAuthenticationOptions
+    {
+        public string Realm { get; set; }
+        public string SigningCertThumbprint { get; set; }
+        public string EncryptionCertStoreName { get; set; }
+        public bool IsPersistent { get; set; }
+        public string IdPEndpoint { get; set; }
+    }
+}
