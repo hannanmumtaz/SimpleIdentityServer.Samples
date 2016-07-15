75ceda7d6301733dbb443665db20a25ef0fb26b7
thabart <habarthierry@hotmail.fr>
2016-07-15 11:55:38 +0200

    Create ES sink

---
+++ Serilog.Console/Event.cs
@@ -0,0 +1,20 @@
+﻿namespace Serilog.Console
+{
+    public class Event
+    {
+        /// <summary>
+        /// Identity the event
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Get or set the category
+        /// </summary>
+        public string Task { get; set; }
+
+        /// <summary>
+        /// Message displayed
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
---
+++ Serilog.Console/EventSource.cs
@@ -0,0 +1,45 @@
+﻿namespace Serilog.Console
+{
+    public class EventSource
+    {
+        private static class Tasks
+        {
+            public const string Authorization = "Authorization";
+        }
+
+        private const string MessagePattern = "{Id} : {Task}, {Message}";
+
+        #region Constructor
+        
+
+        #endregion
+
+        #region Public methods
+
+        public void StartAuthorization(
+            string clientId,
+            string responseType,
+            string scope,
+            string individualClaims)
+        {
+            var evt = new Event
+            {
+                Id = 1,
+                Task = Tasks.Authorization,
+                Message = $"Start the authorization process for the client : {clientId}, response type : {responseType}, scope : {scope} and claims : {individualClaims}"
+            };
+            LogInformation(evt);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void LogInformation(Event evt)
+        {
+            Log.Information(MessagePattern, evt.Id, evt.Task, evt.Message);
+        }
+
+        #endregion
+    }
+}
