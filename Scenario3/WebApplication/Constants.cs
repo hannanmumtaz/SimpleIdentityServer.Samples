891fea7393ccee43e6125414ea2fa77fd427384c
thabart <habarthierry@hotmail.fr>
2016-09-27 21:19:25 +0200

    Ticket 202 : Store callback url as constants

--- Scenario3/WebApplication/Constants.cs
+++ Scenario3/WebApplication/Constants.cs
@@ -7,9 +7,10 @@
         public const string BaseOpenIdUrl = "https://localhost:5443";
         public const string OpenIdConfigurationUrl = BaseOpenIdUrl + "/.well-known/openid-configuration";
         public const string ResourcesUrl = "https://localhost:5444/api/vs/resources";
-        public const string ClientId = "22719e96-2604-418a-815f-6ae406c6ee08";
-        public const string ClientSecret = "05c9a446-5a44-427d-9c93-3908f3a74545";
+        public const string ClientId = "70c665a2-88ed-4dd6-be68-38a7988690fd";
+        public const string ClientSecret = "4551ede1-3cf0-4a1f-b943-e2199875c98f";
         public const string WebApplicationResource = "resources/WebSite";
         public const string CookieWebApplicationName = "CookieWebApplication";
+        public const string Callback = "https://localhost:5105/Authenticate/Callback";
     }
 }
