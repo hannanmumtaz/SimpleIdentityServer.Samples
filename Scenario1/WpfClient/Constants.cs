16e4ca57d3f008105ca38cef2a601d4a53e0c739
thabart <habarthierry@hotmail.fr>
2016-10-05 11:19:32 +0200

    Ticket 205 : Update clientId & secret

--- Scenario1/WpfClient/Constants.cs
+++ Scenario1/WpfClient/Constants.cs
@@ -2,7 +2,7 @@
 {
     internal static class Constants
     {
-        public const string ClientId = "b876ec77-2a32-424a-b187-608f784a65be";
+        public const string ClientId = "bf8ab4cd-9b5f-4d23-8c32-01be1ec7dd72";
         public const string RedirectUrl = "https://client.com";
         public const string UmaConfigurationUrl = "https://localhost:5445/.well-known/uma-configuration";
         public const string BaseOpenidUrl = "https://localhost:5443";
--- Scenario3/WebApplication/Constants.cs
+++ Scenario3/WebApplication/Constants.cs
@@ -7,8 +7,8 @@
         public const string BaseOpenIdUrl = "https://localhost:5443";
         public const string OpenIdConfigurationUrl = BaseOpenIdUrl + "/.well-known/openid-configuration";
         public const string ResourcesUrl = "https://localhost:5444/api/vs/resources";
-        public const string ClientId = "70c665a2-88ed-4dd6-be68-38a7988690fd";
-        public const string ClientSecret = "4551ede1-3cf0-4a1f-b943-e2199875c98f";
+        public const string ClientId = "f06ec666-94cf-4dc4-9d45-b39e2199b4bd";
+        public const string ClientSecret = "da97ac7b-88ca-4856-8ce7-d0e0402c2bbd";
         public const string WebApplicationResource = "resources/WebSite";
         public const string CookieWebApplicationName = "CookieWebApplication";
         public const string Callback = "https://localhost:5105/Authenticate/Callback";
